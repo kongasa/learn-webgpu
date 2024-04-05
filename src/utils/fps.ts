3288709bcac643ced81eb40ef4a3a20e3456d4c8
kong <kongddavid@gmail.com>
2024-04-06 02:59:36 +0800

    fps 2

--- src/utils/fps.ts
+++ src/utils/fps.ts
@@ -1,10 +1,10 @@
 import { Camera } from "./camera";
 import { DVec, multi } from "./vec";
 
-const angV = 1/100000;
-const targetV = 1/100000;
-const rightV = 1/100000;
-const upV = 1/100000;
+const angV = 1 / 1000000;
+const targetV = 1 / 1000000;
+const rightV = 1 / 1000000;
+const upV = 1 / 1000000;
 
 enum AllowedKeyCodes {
   KeyW = "KeyW",
@@ -68,33 +68,39 @@ export class FpsCamera extends Camera {
     super();
   }
   update(dt: number) {
-    if(KeyBit['KeyW']!==KeyBit['KeyS']){
-      if(KeyBit['KeyW']){
-        this.positionByVec(multi(this.target,targetV*dt) as DVec);
+    if (KeyBit["KeyW"] !== KeyBit["KeyS"]) {
+      if (KeyBit["KeyW"]) {
+        this.positionByVec(multi(this.target, targetV * dt) as DVec);
       } else {
-        this.positionByVec(multi(this.target,-targetV*dt) as DVec);
+        this.positionByVec(multi(this.target, -targetV * dt) as DVec);
       }
     }
-    if(KeyBit['KeyA']!==KeyBit['KeyD']){
-      if(KeyBit['KeyA']){
-        this.positionByVec(multi(this.right,-rightV*dt) as DVec);
+    if (KeyBit["KeyA"] !== KeyBit["KeyD"]) {
+      if (KeyBit["KeyA"]) {
+        this.positionByVec(multi(this.right, -rightV * dt) as DVec);
       } else {
-        this.positionByVec(multi(this.right,rightV*dt) as DVec);
+        this.positionByVec(multi(this.right, rightV * dt) as DVec);
       }
     }
-    if(KeyBit['Space']!==KeyBit['KeyC']){
-      if(KeyBit['Space']){
-        this.positionByVec(multi(this.worldUp,upV*dt) as DVec);
+    if (KeyBit["Space"] !== KeyBit["KeyC"]) {
+      if (KeyBit["Space"]) {
+        this.positionByVec(multi(this.worldUp, upV * dt) as DVec);
       } else {
-        this.positionByVec(multi(this.worldUp,-upV*dt) as DVec);
+        this.positionByVec(multi(this.worldUp, -upV * dt) as DVec);
       }
     }
-    if(KeyBit['KeyQ']!==KeyBit['KeyE']){
-      if(KeyBit['KeyQ']){
-
+    if (KeyBit["KeyQ"] !== KeyBit["KeyE"]) {
+      if (KeyBit["KeyQ"]) {
+        this.pitchRad(angV * dt);
       } else {
-        
+        this.pitchRad(-angV * dt);
       }
     }
+  }
+  pitchRad(rad: number) {
+    const [x, y, z] = this.target;
+    const c = Math.cos(rad);
+    const s = Math.sin(rad);
+    this.lookAtVec([c * x + s * z, y, -s * x + c * z, 0]);
   }
 }
